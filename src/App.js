4d9954db9fb937ec94a4ab637a73290a1bd6ff72
Tsvetozar Bonev <tsbonev@gmail.com>
2018-07-21 17:28:06 +0300

    Add project adding

--- src/App.js
+++ src/App.js
@@ -29,10 +29,16 @@ class App extends Component {
     ]})
   }
 
+  handleAddProject(project){
+    let projects = this.state.projects
+    projects.push(project)
+    this.setState({projects:projects})
+  }
+
   render() {
     return (
       <div className="App">
-        <AddProject />
+        <AddProject addProject={this.handleAddProject.bind(this)}/>
         <Projects projects={this.state.projects}/>
       </div>
     );
--- src/components/AddProject.js
+++ src/components/AddProject.js
@@ -1,21 +1,46 @@
 import React, { Component } from 'react';
 
 class AddProject extends Component {
+
+
+    constructor(){
+        super()
+        this.state = {
+            newProject:{}
+        }
+    }
+
   
 static defaultProps = {
     categories: ["Web Design", "Web Developments", "Mobile Development"]
 }
 
+    handleSubmit(e){
+    if(this.refs.title.value === ''){
+        alert('Title is required')
+    } else {
+        this.setState({newProject: {
+            title: this.refs.title.value,
+            category: this.refs.category.value
+        }}, function(){
+            this.props.addProject(this.state.newProject)
+        })
+    }
+    console.log('Sumbitted')
+    e.preventDefault()
+    
+    }
+
   render() {
 
     let categoryOptions = this.props.categories.map(category => {
-        return <option key={category} value="category">{category}</option> 
+        return <option key={category} value={category}>{category}</option> 
     })
 
     return (
         <div>
             <h3>Add project</h3>
-            <form>
+            <form onSubmit={this.handleSubmit.bind(this)}>
                 <div>
                     <label>Title</label>
                     <input type="text" ref="title"/>
@@ -26,6 +51,7 @@ static defaultProps = {
                     </select>
                     <br/>
                 </div>
+                <input type="submit" value="Submit"/>
             </form>
         </div>
     );
